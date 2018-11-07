f8b5b75566ad1fa54221112ca7953d202d4c2f0b
Michael Savage <mikejsavage@gmail.com>
2018-11-07 20:53:04 +0200

    Add newlines to log output

--- log.h
+++ log.h
@@ -3,10 +3,10 @@
 #include "ggformat.h"
 #include "platform_backtrace.h"
 
-#define INFO( form, ... ) ggprint_to_file( stdout, "[INFO] " form, ##__VA_ARGS__ )
-#define WARN( form, ... ) ggprint_to_file( stderr, "[WARN] " form, ##__VA_ARGS__ )
+#define INFO( form, ... ) ggprint_to_file( stdout, "[INFO] " form "\n", ##__VA_ARGS__ )
+#define WARN( form, ... ) ggprint_to_file( stderr, "[WARN] " form "\n", ##__VA_ARGS__ )
 #define FATAL( form, ... ) \
 	do { \
-		ggprint_to_file( stderr, "[FATAL] " form, ##__VA_ARGS__ ); \
+		ggprint_to_file( stderr, "[FATAL] " form "\n", ##__VA_ARGS__ ); \
 		print_backtrace_and_abort(); \
 	} while( 0 )
