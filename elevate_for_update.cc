137c0ebac0906732173854e07f35fbb8a5ebb565
Michael Savage <mikejsavage@gmail.com>
2018-11-08 13:50:27 +0200

    Whoops

--- elevate_for_update.cc
+++ elevate_for_update.cc
@@ -3,6 +3,6 @@
 #include "win32_exec.h"
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, char * szCmdLine, int iCmdShow ) {
-	exec_and_quit( "launch.exe", "--start-update" );
+	exec_and_quit( "cocainediesel.exe", "--start-update" );
 	return 1;
 }
