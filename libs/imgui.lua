1875573fca92ea3e8665b7f01953c1c0d7ca15a2
Michael Savage <mikejsavage@gmail.com>
2018-11-15 14:31:10 +0200

    Fix Windows build

--- libs/imgui.lua
+++ libs/imgui.lua
@@ -1,3 +1,3 @@
 lib( "imgui", { "libs/imgui/imgui.cc", "libs/imgui/imgui_draw.cc", "libs/imgui/imgui_widgets.cc", "libs/imgui/imgui_impl_glfw.cc", "libs/imgui/imgui_impl_opengl2.cc" } )
-msvc_obj_cxxflags( "libs/imgui/imgui_draw.cc", "/DIMGUI_STB_TRUETYPE_FILENAME=\\\"libs/stb/stb_truetype.h\\\" /DIMGUI_STB_RECT_PACK_FILENAME=\"libs/stb/stb_rect_pack.h\"" )
+msvc_obj_cxxflags( "libs/imgui/imgui_draw.cc", "/DIMGUI_STB_TRUETYPE_FILENAME=\\\"libs/stb/stb_truetype.h\\\" /DIMGUI_STB_RECT_PACK_FILENAME=\\\"libs/stb/stb_rect_pack.h\\\"" )
 gcc_obj_cxxflags( "libs/imgui/imgui_draw.cc", "-Wno-maybe-uninitialized -DIMGUI_STB_TRUETYPE_FILENAME=\\\"libs/stb/stb_truetype.h\\\" -DIMGUI_STB_RECT_PACK_FILENAME=\\\"libs/stb/stb_rect_pack.h\\\"" )
