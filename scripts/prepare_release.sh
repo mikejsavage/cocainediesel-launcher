1937f7844cc4f57256101a3ce3dab8f9205ec7ee
Michael Savage <mikejsavage@gmail.com>
2019-03-06 18:38:04 +0200

    Add prepare_release.sh

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow/artifacts/cocaine_diesel_windows.zip
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-g1fxg/artifacts/cocaine_diesel_linux.zip
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-g1fxg/artifacts/cocaine_diesel_base.zip
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher/artifacts/launcher_windows.zip
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher-jbg0q/artifacts/launcher_linux.zip
+
+7z x cocaine_diesel_windows.zip
+7z x cocaine_diesel_linux.zip
+7z x -obase cocaine_diesel_base.zip
+7z x launcher_windows.zip
+7z x launcher_linux.zip
+
+rm cocaine_diesel_windows.zip
+rm cocaine_diesel_linux.zip
+rm cocaine_diesel_base.zip
+rm launcher_windows.zip
+rm launcher_linux.zip
+
+cp ../installer/*.txt .
+rm base/*.md base/findusedtextures.lua
+rm *.exp *.lib **/*.exp **/*.lib
