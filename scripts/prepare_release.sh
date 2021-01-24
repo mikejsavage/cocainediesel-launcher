6ebced8ea51dff84196fc3c2a7df531f50f581c6
Michael Savage <mikejsavage@gmail.com>
2021-01-24 22:45:00 +0200

    Download zips in parallel and get base from github in release scripts

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -1,22 +1,29 @@
 #! /bin/sh
 
-wget https://ci.appveyor.com/api/projects/mikejsavage/forksow/artifacts/cocaine_diesel_windows.zip
-wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-g1fxg/artifacts/cocaine_diesel_linux.zip
-wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-g1fxg/artifacts/cocaine_diesel_base.zip
-wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher/artifacts/launcher_windows.zip
-wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher-jbg0q/artifacts/launcher_linux.zip
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow/artifacts/cocaine_diesel_windows.zip &
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-g1fxg/artifacts/cocaine_diesel_linux.zip &
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher/artifacts/launcher_windows.zip &
+wget https://ci.appveyor.com/api/projects/mikejsavage/forksow-launcher-jbg0q/artifacts/launcher_linux.zip &
+
+version=$(basename $(pwd))
+wget -O base.zip https://github.com/mikejsavage/forksow/archive/v$version.zip &
+
+wait
 
 7z x cocaine_diesel_windows.zip
 7z x cocaine_diesel_linux.zip
-7z x -obase cocaine_diesel_base.zip
 7z x launcher_windows.zip
 7z x launcher_linux.zip
 
+7z x base.zip forksow-$version/base
+mv forksow-$version/base .
+rmdir forksow-$version
+
 rm cocaine_diesel_windows.zip
 rm cocaine_diesel_linux.zip
-rm cocaine_diesel_base.zip
 rm launcher_windows.zip
 rm launcher_linux.zip
+rm base.zip
 
 cp ../installer/*.txt .
 rm base/*.md base/.gitignore
