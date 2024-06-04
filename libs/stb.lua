a4e29ede047e50989199c461875ec86776cf32fd
Michael Savage <mike@mikejsavage.co.uk>
2024-06-04 10:37:45 +0300

    Compile minimal stb_image, disable all warnings in GCC

--- libs/stb.lua
+++ libs/stb.lua
@@ -1,3 +1,4 @@
 lib( "stb_image", { "libs/stb/stb_image.cc" } )
-msvc_obj_cxxflags( "libs/stb/stb_image%.cc", "/wd4244 /wd4456" )
-gcc_obj_cxxflags( "libs/stb/stb_image%.cc", "-Wno-shadow -Wno-implicit-fallthrough" )
+obj_cxxflags( "libs/stb/stb_image.cc", "-DSTBI_NO_BMP -DSTBI_NO_GIF -DSTBI_NO_HDR -DSTBI_NO_LINEAR -DSTBI_NO_PIC -DSTBI_NO_PNM -DSTBI_NO_PSD -DSTBI_NO_TGA" )
+msvc_obj_cxxflags( "libs/stb/stb_image.cc", "/O2 /wd4244 /wd4456" )
+gcc_obj_cxxflags( "libs/stb/stb_image.cc", "-O2 -w" )
